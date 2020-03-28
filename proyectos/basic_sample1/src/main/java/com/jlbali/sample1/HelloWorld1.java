53e6a1d74dc6072942b5b4d44e6af21e097e48b3
lbali <juan.lucas.bali@gmail.com>
2020-03-28 14:08:21 -0300

    Proyecto en Eclipse

---
+++ proyectos/basic_sample1/src/main/java/com/jlbali/sample1/HelloWorld1.java
@@ -0,0 +1,12 @@
+package com.jlbali.sample1;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
+
+
